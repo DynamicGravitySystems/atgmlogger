03be15cc72721925fbdd9ab6e0bae54470356404
root <root@dgs-ep-z50-centos.ad.dynamicgravitysystems.com>
2017-06-06 19:06:47 -0600

    Minor updates to data_sender utility program, working on improving features with optional arguments

--- datalogger/data_sender.py
+++ datalogger/data_sender.py
@@ -18,6 +18,7 @@ def get_handle(device=None, baudrate=57600):
     if device is None:
         try:
             device = comports()[0].device
+            print("Sending data via comport: {}".format(device))
         except serial.SerialException:
             print('Error determining port to open')
             return 1
@@ -26,18 +27,36 @@ def get_handle(device=None, baudrate=57600):
     return serial_hdl
 
 
-def run(source, rate=1):
+def run(source, rate=.1):
     data = open(source, 'r', encoding='utf-8')
+    send_count = 0
     ser_hdl = get_handle()
     line = data.readline()
     while line is not '':
-        print("sending line: {}".format(line))
-        sendline(ser_hdl, line)
-        time.sleep(rate)
-        line = data.readline()
+        try:
+            print("sending line: {}".format(line))
+            sendline(ser_hdl, line)
+            send_count +=1
+            time.sleep(rate)
+            line = data.readline()
+        except KeyboardInterrupt:
+            print("\nInterrupted - Total sent: {}".format(send_count))
+            print("Cleaning up and exiting")
+            sendline(ser_hdl, '\n')
+            ser_hdl.close()
+            return 0
     sendline(ser_hdl, '\n')
-    print("Exhausted data")
+    print("Total Sent: {}".format(send_count))
+    print("Exhausted data, exiting")
     ser_hdl.close()
+
+def main(argv=None):
+    parser = argparse.ArgumentParser(prog="SerialSender", description="Send arbitrary data over a serial port")
+
+
+
+
+
 
 if __name__ == "__main__":
     run('data.txt')
